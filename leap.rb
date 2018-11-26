7b628cf7f3d7e6e6309e50154e903dd2d2bc0297
AldairMC <aldairmc2023@gmail.com>
2018-11-25 22:01:04 -0500

    Leap v1.5 - one failed

--- leap.rb
+++ leap.rb
@@ -4,12 +4,17 @@ end
 # Leap
 class Year
   def self.leap?(year)
-    if year % 4 == 0
+    if year % 4 == 0 || year % 400 == 0
       return true
-    elsif year % 100 == 0 && year % 400 == 0
-        return false
-    else
-        return false
+    elsif year % 4 == 0 && year % 100 == 0
+      return false
     end
+    # if year % 4 == 0
+    #   return true
+    # elsif year % 100 == 0 && year % 400 == 0
+    #     return false
+    # else
+    #     return false
+    # end
   end
 end
